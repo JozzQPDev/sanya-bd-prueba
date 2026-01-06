d824b48420dd1168098fb575c9a85cdc0e96daf7
Jhon03-bit <beckperez07@gmail.com>
2026-01-06 12:26:35 -0500

    Actualizar datos de la empresa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // Esto hace que se consulte la DB en cada visita
+  adapter: vercel(),
+});
